7bc2b723bc470f766b63cbdaad82ac7254be5a14
nevergiveup2212 <blackcat22121996@gmail.com>
2018-01-11 17:35:08 +0700

    create user

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ApplicationRecord
-	validates :image, presence: true
+	validates :user_id, :image, presence: true
 
  	has_attached_file :image, styles: { :med => "640x640" }
   	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+  	belongs_to :user
 end
